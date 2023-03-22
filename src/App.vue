5e4e0503c39b1971ab5c05cdc067310f04221dfb
oguzhannsaritas <oguzhan_saritass@hotmail.com>
2023-03-22 17:44:36 +0300

    copy-fixx

--- src/App.vue
+++ src/App.vue
@@ -6,26 +6,11 @@
 </template>
 
 <style >
-*{
-  margin: 0;
-  border: 0;
-  padding: 0;
-  font-family: sans-serif;
-}
 
 
 
-#app {
-  width: 100vw;
-  height: 100vh;
-  background: black;
-  display: flex;
-  flex-direction: column;
 
-}
-a {
-  text-decoration: none;
-}
+
 
 
 </style>
